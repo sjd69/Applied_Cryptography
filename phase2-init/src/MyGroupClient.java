42ad126ec29f7b7f9a194de96785311cd72248cb
NohrianScum <ruz24@pitt.edu>
2017-10-01 18:46:18 -0400

    Started fleshing this out
    I'm using GroupClient.java as a template. I'm not sure what else needs to be changed... for the token, I'm assuming not much.
    Does this format look okay? If anything needs to be changed or fixed, feel free to make necessary edits.

--- phase2-init/src/MyGroupClient.java
+++ phase2-init/src/MyGroupClient.java
@@ -1,13 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.ObjectInputStream;
 
 public class MyGroupClient
 {
-	GroupClient gorupclient = new GroupClient();
+	GroupClient groupclient = new GroupClient();
 	
 	public boolean startMyGroupClient(String server, int port)
 	{
 		if (fileclient.connect(server, port))
 		{
 			// get token
+			try {
+				Token userToken = null;
+				Envelope message = null, response = null;
+				
+				// Tell the server to return a token
+				message = new Envelope("GET");
+				message.addObject(username); // add username String
+				output.writeObject(message);
+				
+				// Get the response from the server
+				response = (Envelope)input.readObject();
+				
+				// Successful response
+				if(response.getMessage().equals("OK")) {
+					// If there is a token in the envelope, return it
+					ArrayList<Object> temp = null;
+					temp = response.getObjContents();
+					
+					if (temp.size() == 1) {
+						userToken = (Token)temp.get(0);
+						return userToken;
+					}
+				}
+				return null;
+			}
+			catch(Exception e) {
+				System.err.println("Error: " + e.getMessage());
+				e.printStackTrace(System.err);
+				return null;
+			}
+			
 			// create user
 			// delete user
 			// create group
