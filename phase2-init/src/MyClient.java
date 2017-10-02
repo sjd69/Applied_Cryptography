a3bfce79ee438e6b454b0cefa15a7c95069273c8
sjd69 <sjd69@pitt.edu>
2017-10-02 11:23:02 -0400

    Started work on MyClient implementation

--- phase2-init/src/MyClient.java
+++ phase2-init/src/MyClient.java
@@ -5,6 +5,10 @@ public class MyClient {
         FileClient fileClient = new FileClient();
         GroupClient groupClient = new GroupClient();
         UserToken userToken = null;
+        String username = null;
+
+        MyFileClient myFileClient = new MyFileClient();
+        MyGroupClient myGroupClient = new MyGroupClient();
 
         Scanner scanIn = new Scanner(System.in);
 
@@ -14,15 +18,69 @@ public class MyClient {
 
         do {
             while (!auth) {
-                System.out.println("MENU\n 1: LOGIN\n 2: EXIT");
+                System.out.println("MENU\n " +
+                        "1: LOGIN\n " +
+                        "2: EXIT");
                 nav = scanIn.nextInt();
                 if (nav == 1) {
                     exit = false;
 
+                    System.out.println("Enter username");
+                    username = scanIn.nextLine();
+
                     //Connect to group client to auth
-                    groupClient.connect();
+                    //Not entirely sure what to use for port
+                    groupClient.connect("localhost", 5309 );
+
+                    if (groupClient.isConnected()) {
+                        userToken = groupClient.getToken(username);
+
+                        if (userToken == null) {
+                            System.out.println("Authentication error.");
+                        } else {
+                            auth = true;
+                            System.out.println("Authentication successful.");
+                        }
+                        groupClient.disconnect();
+
+                    } else {
+                        System.out.println("Connection error.");
+                        exit = true;
+                        break;
+                    }
                 } else if (nav == 2) {
                     exit = true;
+                    break;
+                }
+            }
+
+            while (auth) {
+                System.out.println("MENU:\n" +
+                        "1: Connect to File Server\n" +
+                        "2: Connect to Group Server\n" +
+                        "3: Exit\n");
+
+
+                switch(nav) {
+                    case 1:
+                        myFileClient.startMyFileClient("localhost", 5309, (Token)userToken);
+                        break;
+
+                    case 2:
+                        myGroupClient.startMyGroupClient("localhost", 5309, (Token)userToken);
+                        break;
+
+                    case 3:
+                        System.out.println("Exiting...");
+                        exit = true;
+                        break;
+
+                    default:
+                        System.out.println("Invalid input.");
+                        break;
+                }
+
+                if (exit) {
                     break;
                 }
             }
