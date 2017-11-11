dd93acca3ba3326255efaadae54eb93a2c723542
NohrianScum <ruz24@pitt.edu>
2017-11-10 20:02:20 -0500

    Create Server.java

---
+++ phase4-init/src/Server.java
@@ -0,0 +1,24 @@
+import java.net.Socket;
+import java.security.InvalidKeyException;
+
+public abstract class Server {
+	
+	protected int port;
+	public String name;
+	abstract void start() throws InvalidKeyException;
+	
+	public Server(int _SERVER_PORT, String _serverName) {
+		port = _SERVER_PORT;
+		name = _serverName; 
+	}
+	
+		
+	public int getPort() {
+		return port;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
