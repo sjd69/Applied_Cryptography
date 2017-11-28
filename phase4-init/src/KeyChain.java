83a87b8bfc5a9d4a3e092c3a0d1364118f355227
Kadie Clancy <kdc42@pitt.edu>
2017-11-28 01:53:15 -0500

    updates to keychain

--- phase4-init/src/KeyChain.java
+++ phase4-init/src/KeyChain.java
@@ -1,19 +1,22 @@
 import javax.crypto.SecretKey;
 import java.util.ArrayList;
+import javax.crypto.spec.IvParameterSpec;
 
 public class KeyChain implements java.io.Serializable {
 	private static final long serialVersionUID = 4L;
-    private ArrayList<KeySet> keychain;
+    private ArrayList<SerKeySet> keychain;
 	private String groupName;
     
-    public KeyChain(String groupName) {
+    
+	public KeyChain(String groupName) {
         this.groupName = groupName;
-        this.keychain = new ArrayList<KeySet>();
+        this.keychain = new ArrayList<SerKeySet>();
     }
 	
 	// Add newly generated key to the group keychain
     public void addNewKey(KeySet key){
-    	this.keychain.add(key);
+    	SerKeySet sks = new SerKeySet(key.getKey(), key.getIv().getIV());
+    	this.keychain.add(sks);
     }
     
     // Return the group name of the keychain
@@ -23,7 +26,9 @@ public class KeyChain implements java.io.Serializable {
     
     // Return the last (freshest) key that will be used for encryption
     public KeySet getEncryptionKey() {
-        return keychain.get(keychain.size() - 1);
+        SerKeySet skc = keychain.get(keychain.size() - 1);
+        KeySet ks = new KeySet(skc.getKey(), new IvParameterSpec(skc.getIv()));
+        return ks;
     }
     
     // Return the index of last (freshest) key that will be used for encryption
@@ -33,6 +38,8 @@ public class KeyChain implements java.io.Serializable {
 
 	// Returns the key for encryption, based on the version number
     public KeySet getDecryptionKey(int version) {
-        return keychain.get(version);
+        SerKeySet skcd = keychain.get(version);
+        KeySet ksd = new KeySet(skcd.getKey(), new IvParameterSpec(skcd.getIv()));
+        return ksd;
     }
 }
