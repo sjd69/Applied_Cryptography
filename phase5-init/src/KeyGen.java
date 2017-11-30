958f451a6ddaf38a1f9a795c2f0f3f32dd267a68
NohrianScum <ruz24@pitt.edu>
2017-11-30 18:19:26 -0500

    Create KeyGen.java

---
+++ phase5-init/src/KeyGen.java
@@ -0,0 +1,36 @@
+import java.io.StringWriter;
+import java.security.*;
+import java.util.Scanner;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+
+public class KeyGen {
+
+    public static void main(String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
+        PrivateKey rsaPrivate = null;
+        PublicKey rsaPublic = null;
+        System.out.println("---- RSA Key Pair Generation ----");
+        try {
+            KeyPairGenerator rsaGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+            rsaGenerator.initialize(2048);
+            KeyPair rsaKeys = rsaGenerator.generateKeyPair();
+            rsaPrivate = rsaKeys.getPrivate();
+            rsaPublic = rsaKeys.getPublic();
+
+            if (rsaPrivate != null && rsaPublic != null) {
+                System.out.println("RSA keys successfully generated.");
+                System.out.println("\nPRIVATE: "
+                        + new String(java.util.Base64.getEncoder().encode(rsaPrivate.getEncoded())));
+                System.out.println("\nPUBLIC: "
+                        + new String(java.util.Base64.getEncoder().encode(rsaPublic.getEncoded())));
+                System.out.println("\nDO NOT GIVE YOUR PRIVATE KEY TO ANYONE!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
